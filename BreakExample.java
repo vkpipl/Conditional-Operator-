9790fe0a75c39ff9d1ed7eabf74bffea2416fd33
vkpipl <135228134+vkpipl@users.noreply.github.com>
2024-01-02 20:46:33 +0530

    Add files via upload

---
+++ BreakExample.java
@@ -0,0 +1,15 @@
+class BreakExample
+{
+public static void main(String args[])
+{
+for(int i=1;i<=10;i++)
+{
+if(i==5)
+{
+System.out.println("Breaking the loop at i=5");
+break;
+}
+System.out.println(i);
+}
+}
+}
