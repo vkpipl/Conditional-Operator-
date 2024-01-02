f8ecb38894030a08e5447deaf5e724b858b1d653
vkpipl <135228134+vkpipl@users.noreply.github.com>
2024-01-02 20:47:47 +0530

    Add files via upload

---
+++ ContinueExample.java
@@ -0,0 +1,15 @@
+class ContinueExample
+{
+public static void main(String args[])
+{
+for(int i=1;i<=5;i++)
+{
+if(i==3)
+{
+//System.out.println("Skipping iteration at i==3");
+continue;
+}
+System.out.println(i);
+}
+}
+}
